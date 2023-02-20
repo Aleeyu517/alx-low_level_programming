df71ea2ef80728405a8b39913a698d09f58287c0
Aleeyu517 <aliyumuhammad517@gmail.com>
2023-02-20 10:32:56 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - A program that prints a line using the printf function
  * Return: Always 0
  */
-
 int main(void)
+
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
