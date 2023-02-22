9d7963739e7279e64170a492abc2aaa9d9073fc7
Aleeyu517 <aliyumuhammad517@gmail.com>
2023-02-21 18:05:11 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(void)
-{
-	char str[] = "_putchar\n";
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		putchar(str[i]);
-	}
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
