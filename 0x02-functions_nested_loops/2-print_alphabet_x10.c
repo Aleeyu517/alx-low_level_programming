c42786767d948cf5e85bb17fd54af8ba21343858
Aleeyu517 <aliyumuhammad517@gmail.com>
2023-02-23 01:05:34 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints allalphabets in lowercase 10 times
+ * Return: On success 1.
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char i, k;
+
+	for (k = 0; k < 10; k++)
+
+	{
+		for (i = 'a'; i <= 'z'; i++)
+
+		{
+			_putchar(i);
+
+		}
+
+		_putchar('\n');
+
+	}
+
+}
